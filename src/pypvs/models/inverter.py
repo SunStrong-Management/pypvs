2df8f0a24d806056f9bd8596a2620e0292296aee
Marcin Jabrzyk <marcin@conclusive.pl>
2025-10-30 13:12:04 +0100

    Introduce MPPT data for DC measurements
    Provide new fields to PVSInverter so the DC data are extracted from the varserver too.
    
    Closes #12

--- src/pypvs/models/inverter.py
+++ src/pypvs/models/inverter.py
@@ -4,28 +4,59 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from datetime import datetime, timezone
-from typing import Any
+from typing import Any, Optional
 
 
 @dataclass(slots=True)
 class PVSInverter:
-    """Model for an Enphase microinverter."""
+    """
+    Model for an Enphase microinverter.
+
+    Attributes:
+        serial_number (str): The serial number of the inverter.
+        model (str): The model name or number of the inverter.
+        last_report_date (Optional[datetime | str]): The timestamp of the
+            last report, as a datetime object or string.
+        last_report_kw (Optional[float]): The last reported AC power
+            output in kilowatts.
+        last_report_voltage_v (Optional[float]): The last reported AC
+            voltage in volts.
+        last_report_current_a (Optional[float]): The last reported AC
+            current in amperes.
+        last_report_frequency_hz (Optional[float]): The last reported AC
+            frequency in hertz.
+        last_report_temperature_c (Optional[float]): The last reported
+            temperature in degrees Celsius.
+        lte_kwh (Optional[float]): The lifetime energy produced by the
+            inverter in kilowatt-hours.
+        last_mppt_voltage_v (Optional[float]): The DC voltage from the panel.
+        last_mppt_current_a (Optional[float]): The DC current from the panel.
+        last_mppt_power_kw (Optional[float]): The DC power from the panel.
+
+    Methods:
+        from_varserver(data: dict[str, Any]) -> PVSInverter:
+            Class method to initialize a PVSInverter instance from a
+            dictionary of varserver variables.
+            Converts the date string in ISO 8601 format to a UTC timestamp.
+    """
 
     serial_number: str
     model: str
-    last_report_date: int
-    last_report_kw: float
-    last_report_voltage_v: float
-    last_report_current_a: float
-    last_report_frequency_hz: float
-    last_report_temperature_c: float
-    lte_kwh: float
+    last_report_date: Optional[datetime | str] = None
+    last_report_kw: Optional[float] = None
+    last_report_voltage_v: Optional[float] = None
+    last_report_current_a: Optional[float] = None
+    last_report_frequency_hz: Optional[float] = None
+    last_report_temperature_c: Optional[float] = None
+    lte_kwh: Optional[float] = None
+    last_mppt_voltage_v: Optional[float] = None
+    last_mppt_current_a: Optional[float] = None
+    last_mppt_power_kw: Optional[float] = None
 
     @classmethod
     def from_varserver(cls, data: dict[str, Any]) -> PVSInverter:
-        """
-        Initialize from /sys/devices/inverter/*/* varserver variables packed in JSON.
-        """
+        """Initialize from /sys/devices/inverter/*/* varserver variables
+        packed in JSON to a PVSInverter instance."""
 
         # Convert date from format "2024-09-30T16:15:00Z" to UTC seconds
         date_str = data["msmtEps"]
@@ -44,4 +75,7 @@ class PVSInverter:
             last_report_frequency_hz=data["freqHz"],
             last_report_temperature_c=data["tHtsnkDegc"],
             lte_kwh=data["ltea3phsumKwh"],
+            last_mppt_voltage_v=data.get("vMppt1V"),
+            last_mppt_current_a=data.get("iMppt1A"),
+            last_mppt_power_kw=data.get("pMppt1Kw"),
         )
